8f5104733f9a6a9434a7ceb62f3219caa4fb0a0b
Rajul <Rajulverma7777@gmail.com>
2022-10-05 23:44:38 +0530

    stored user data in localstorage

--- BMH-Client/src/services/auth.service.ts
+++ BMH-Client/src/services/auth.service.ts
@@ -5,7 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   AuthToken = localStorage.getItem('11510199114101116');
-  constructor() {}
+  UserDetails = localStorage.getItem('99105112104101114116101120116');
+
+  //! SETTING USER DATA IN LOCAL STORAGE
+  SetUserDataLocal(Data: any) {
+    localStorage.setItem('99105112104101114116101120116', Data);
+  }
+  GetUserDataLocal() {
+    return JSON.stringify(this.UserDetails);
+  }
+
+  //! MANAGING AUTHTOKEN IN LOCAL STORAGE
   SetLocalAuthToken(Token: any) {
     localStorage.setItem('11510199114101116', Token);
   }
@@ -14,5 +24,6 @@ export class AuthService {
   }
   RemoveLocalAuthToken() {
     localStorage.removeItem('11510199114101116');
+    localStorage.removeItem('99105112104101114116101120116');
   }
 }
